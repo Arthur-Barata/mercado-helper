4cccc2fe43b43611f23640f3375eff6aaa4abb77
Arthur Barata <arthurbs@motorola.com>
2023-02-24 21:40:38 -0400

    teste

--- src/componets/cards/index.tsx
+++ src/componets/cards/index.tsx
@@ -1,34 +1,32 @@
-import Container from "./styles"
+import Container from "./styles";
 
-interface cardsProps{
-    produto:string
-    imgSrc:string
-    valor:number
-    quantidade: number
-
-
+export interface cardsProps {
+  produto: string;
+  imgSrc: string;
+  valor: number;
+  quantidade: number;
 }
 
+function Cards(props: cardsProps) {
+  return (
+    <Container>
+      <div>
+        <img src={props.imgSrc}></img>
+      </div>
 
- function Cards (props:cardsProps){
-
-
-    return(
-        <Container> 
-            <div>
-                <img src={props.imgSrc}></img>
-             </div>
-               
-                <h2>{props.produto}  </h2> 
-                <li> <strong>Valor:</strong> R$ {props.valor}</li>
-                <li><strong>Quantidade:</strong> {props.quantidade}</li> 
-                <li><strong>Total: </strong>R$ {props.quantidade *props.valor}</li> 
-        </Container>
-        
-    )
-
-
-
+      <h2>{props.produto} </h2>
+      <li>
+        {" "}
+        <strong>Valor:</strong> R$ {props.valor}
+      </li>
+      <li>
+        <strong>Quantidade:</strong> {props.quantidade}
+      </li>
+      <li>
+        <strong>Total: </strong>R$ {props.quantidade * props.valor}
+      </li>
+    </Container>
+  );
 }
 
 export default Cards;
---
+++ src/componets/cardsList/styles.tsx
@@ -0,0 +1,8 @@
+import styled from "styled-components";
+
+export default styled.div`
+  display: flex;
+  justify-content: center;
+  gap: 1.5rem;
+  flex-wrap: wrap;
+`;
